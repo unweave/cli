bd576870fcd3bde9c660dca9768229caaab1bc6b
Noorvir Aulakh <noorvir.aulakh@gmail.com>
2022-02-14 00:58:17 +0100

    Add locally linked unweave project to list command output

--- cmd/list.go
+++ cmd/list.go
@@ -7,6 +7,17 @@ import (
 	"github.com/unweave/cli/entity"
 )
 
+func findLinkedPath(projects map[string]entity.ProjectConfig, projectId string) (
+	string, bool,
+) {
+	for path, project := range projects {
+		if project.Id == projectId {
+			return path, true
+		}
+	}
+	return "", false
+}
+
 func (h *Handler) List(ctx context.Context, cmd *entity.Command) error {
 	projects, err := h.ctrl.GetProjects(ctx)
 	if err != nil {
@@ -15,7 +26,11 @@ func (h *Handler) List(ctx context.Context, cmd *entity.Command) error {
 
 	fmt.Println("Projects:")
 	for _, p := range projects {
-		fmt.Println("\t", p)
+		if path, ok := findLinkedPath(h.cfg.Root.Projects, p.Id); ok {
+			fmt.Printf("  %s \n\tID: %s \n\tPath: %s\n", p.Name, p.Id, path)
+		} else {
+			fmt.Printf("  %s \n\tID: %s \n\tPath: Not linked\n", p.Name, p.Id)
+		}
 	}
 
 	if (h.cfg.Root.Projects == nil) || (len(h.cfg.Root.Projects) == 0) {
