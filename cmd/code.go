9c6a7e1c479738cf6d4414bd25de407eb32554d2
Duarte OC <duarteocarmo@gmail.com>
2023-05-03 13:26:35 +0100

    Refactor of code command for similar behaviour to ssh command (#1)
    * adds similar behaviour to ssh
    
    * remove useless args

--- cmd/code.go
+++ cmd/code.go
@@ -7,6 +7,7 @@ import (
 	"os/exec"
 
 	"github.com/spf13/cobra"
+	"github.com/unweave/cli/config"
 	"github.com/unweave/cli/ui"
 	"github.com/unweave/unweave/api/types"
 )
@@ -15,36 +16,74 @@ func Code(cmd *cobra.Command, args []string) error {
 	cmd.SilenceUsage = true
 	ctx := cmd.Context()
 
-	// TODO: parse args and forward to ssh command
+	execCh := make(chan types.Exec)
+	errCh := make(chan error)
 
-	ui.Infof("Initializing node...")
+	var err error
+	var execRef string // Can be execID or name
 
-	execch, errch, err := execCreateAndWatch(ctx, types.ExecConfig{}, types.GitConfig{})
-	if err != nil {
-		return err
+	if config.CreateExec {
+
+		ui.Infof("Initializing node...")
+
+		execCh, errCh, err = execCreateAndWatch(ctx, types.ExecConfig{}, types.GitConfig{})
+		if err != nil {
+			return err
+		}
+
+	} else {
+
+		if execRef == "" {
+			var execs []types.Exec
+
+			execRef, execs, err = selectExec(cmd.Context(), "Select a session to connect to")
+			if err != nil {
+				return err
+			}
+			if len(execs) == 0 {
+				ui.Errorf("❌ No active sessions found and no session name or ID provided. If " +
+					"you want to create a new session, use the --new flag.")
+				os.Exit(1)
+			}
+		}
+
+		execCh, errCh, err = execWaitTillReady(ctx, execRef)
+		if err != nil {
+			return err
+		}
 	}
-
 	for {
 		select {
-		case e := <-execch:
+		case e := <-execCh:
 			if e.Status == types.StatusRunning {
+
 				if e.Connection == nil {
 					ui.Errorf("❌ Something unexpected happened. No connection info found for session %q", e.ID)
 					ui.Infof("Run `unweave session ls` to see the status of your session and try connecting manually.")
 					os.Exit(1)
 				}
 				ui.Infof("🚀 Session %q up and running", e.ID)
-				ui.Infof("Setting up VS Code ...")
+				ui.Infof("🔧 Setting up VS Code ...")
 
 				if err := removeKnownHostsEntry(e.Connection.Host); err != nil {
 					// Log and continue anyway. Most likely the entry is not there.
 					ui.Debugf("Failed to remove known_hosts entry: %v", err)
 				}
+
 				if err := addHost("uw:"+e.ID, e.Connection.Host, e.Connection.User, e.Connection.Port); err != nil {
 					ui.Debugf("Failed to add host to ssh config: %v", err)
 				}
 
+				defer func() {
+					if e := removeHost("uw:" + e.ID); e != nil {
+						ui.Debugf("Failed to remove host from ssh config: %v", e)
+					}
+				}()
+
+				// TODO we should wait until port is open
+
 				arg := fmt.Sprintf("vscode-remote://ssh-remote+%s@%s/home/ubuntu", e.Connection.User, e.Connection.Host)
+
 				codeCmd := exec.Command("code", "--folder-uri="+arg)
 				codeCmd.Stdout = os.Stdout
 				codeCmd.Stderr = os.Stderr
@@ -53,11 +92,11 @@ func Code(cmd *cobra.Command, args []string) error {
 					ui.Errorf("Failed to start VS Code: %v", e)
 					os.Exit(1)
 				}
-				ui.Successf("VS Code is ready!")
+				ui.Successf("✅ VS Code is ready!")
 				return nil
 			}
 
-		case err := <-errch:
+		case err := <-errCh:
 			var e *types.Error
 			if errors.As(err, &e) {
 				uie := &ui.Error{Error: e}
@@ -70,4 +109,5 @@ func Code(cmd *cobra.Command, args []string) error {
 			return nil
 		}
 	}
+
 }
