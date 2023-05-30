c32f073bfef081d278bcd51a3cef4c1dc73925e4
Callum Dempsey Leach <callum.leach@hotmail.co.uk>
2023-05-30 16:30:21 +0100

    Change host dirpath to /ml-project

--- config/dirpath.go
+++ config/dirpath.go
@@ -1,4 +1,4 @@
 package config
 
 // UnweaveHostDir is the filesystem location where your code gets stored on the host. Adjust this as you need.
-const UnweaveHostDir = "/home/unweave"
+const UnweaveHostDir = "/home/unweave/ml-project"
