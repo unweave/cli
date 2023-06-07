215c1df0b4b04f0b3ce536060005a113685a9728
Callum Dempsey Leach <callum.leach@hotmail.co.uk>
2023-06-07 09:59:02 +0100

    Faster SCP Copying for Local to Remote directories (#18)
    * Handle a case where the exec connection might be nil
    
    * Check if the first argument of SCP points towards a logical directory:
      1. If it does, assume we are doing local to remote (as the first argument would use the 'sess' header otherwise.
      2. DRY up the Copy arg to Scp args so we can prune the remote dirpath in a way that's re-usable, there's no guarantee that's the first SCP arg, but there is a guarantee it is the second one post validation.
    
    * Address feedback

--- cmd/copy.go
+++ cmd/copy.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"context"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -17,68 +16,105 @@ import (
 var getSessionIDRegex = regexp.MustCompile(`^sess:([^/]+)`)
 
 func Copy(cmd *cobra.Command, args []string) error {
-	scpArgs := make([]string, 0, len(args))
-	var targetExec *types.Exec
+	exec, err := getTargetExec(cmd, args)
+	if err != nil {
+		return err
+	}
 
+	if exec == nil {
+		return fmt.Errorf("At least one remote host must be specified")
+	}
+	if exec.Connection == nil {
+		return fmt.Errorf("Target session must have an active connection")
+	}
+	if exec.SSHKey.PublicKey == nil && config.SSHPublicKeyPath == "" {
+		return fmt.Errorf("Failed to identify public key, check your Unweave config file or specify it manually")
+	}
+
+	scpArgs, err := formatPaths(exec, args)
+	if err != nil {
+		ui.Infof("❌ Unsuccessful copy: %s", err.Error())
+	}
+	ui.Infof(fmt.Sprintf("🔄 Copying %s => %s", scpArgs[0], scpArgs[1]))
+
+	switch {
+	case isLocalDirToRemoteCopy(args):
+		err = copySourceAndUnzip(exec.ID, scpArgs[0], splitSessFromDirpath(args[1]), *exec.Connection, *exec.SSHKey.PublicKey)
+	default:
+		err = copySourceSCP(scpArgs[0], scpArgs[1], *exec.SSHKey.PublicKey)
+	}
+
+	if err != nil {
+		ui.Infof("❌ Unsuccessful copy %s => %s", scpArgs[0], scpArgs[1])
+		return nil
+	}
+
+	ui.Infof("✅  Copied %s => %s", scpArgs[0], scpArgs[1])
+	return nil
+}
+
+func isLocalDirToRemoteCopy(args []string) bool {
+	if strings.Contains(args[0], "sess:") {
+		return false
+	}
+
+	pathInfo, err := os.Stat(args[0])
+	if err != nil || pathInfo == nil {
+		return false
+	}
+
+	return pathInfo.IsDir()
+}
+
+func isRemoteDirToLocalCopy(args []string) bool {
+	if strings.Contains(args[1], "sess:") {
+		return false
+	}
+
+	pathInfo, err := os.Stat(args[1])
+	if err != nil || pathInfo == nil {
+		return false
+	}
+
+	return pathInfo.IsDir()
+}
+
+func getTargetExec(cmd *cobra.Command, args []string) (*types.Exec, error) {
+	var targetExec *types.Exec
 	for _, arg := range args {
 		argExecID, err := getExecIDFromCopyArgs(arg)
 		if err != nil {
-			ui.HandleError(err)
-			os.Exit(1)
+			return nil, err
 		}
 
 		if argExecID != "" && targetExec != nil {
-			return fmt.Errorf("Copying between multiple sessions is not supported")
+			return nil, fmt.Errorf("Copying between multiple sessions is not supported")
 		}
 
 		if argExecID != "" {
 			targetExec, err = getExecByNameOrID(cmd.Context(), argExecID)
 			if err != nil {
-				return fmt.Errorf("Could not find session by name or ID")
+				return nil, fmt.Errorf("Could not find session by name or ID")
 			}
 		}
-
-		formattedArg, err := formatCopyArgToScpArgs(cmd.Context(), argExecID, targetExec, arg)
-		if err != nil {
-			ui.HandleError(err)
-			os.Exit(1)
-		}
-
-		scpArgs = append(scpArgs, formattedArg)
 	}
-	if targetExec == nil {
-		return fmt.Errorf("At least one remote host must be specified")
-	}
-
-	if targetExec.SSHKey.PublicKey == nil && config.SSHPublicKeyPath == "" {
-		return fmt.Errorf("Failed to identify public key, check your Unweave config file or specify it manually")
-	}
-
-	publicKeyPath := ""
-	if config.SSHPrivateKeyPath != "" {
-		publicKeyPath = config.SSHPublicKeyPath
-	} else {
-		publicKeyPath = *targetExec.SSHKey.PublicKey
-	}
-
-	ui.Infof(fmt.Sprintf("🔄 Copying %s => %s", scpArgs[0], scpArgs[1]))
-	err := copySourceSCP(scpArgs[0], scpArgs[1], publicKeyPath)
-	ui.Infof("✅  Copied %s => %s", scpArgs[0], scpArgs[1])
-	return err
+	return targetExec, nil
 }
 
-func formatCopyArgToScpArgs(ctx context.Context, argExecID string, exec *types.Exec, arg string) (string, error) {
-	if argExecID == "" {
-		if arg == "." {
-			return os.Getwd()
+func formatLocalPath(arg string) (string, error) {
+	if arg == "." {
+		arg, err := os.Getwd()
+		if err != nil {
+			return "", fmt.Errorf("Could not get local directory")
 		}
-
 		return arg, nil
 	}
+	return arg, nil
+}
 
+func formatRemotePath(exec *types.Exec, arg string) (string, error) {
 	if exec == nil {
-		return "", fmt.Errorf("Assertion failed, please file an issue with the Unweave team." +
-			"Please provide steps to reproduce")
+		return "", fmt.Errorf("Assertion failed, please file an issue with the Unweave team. Please provide steps to reproduce")
 	}
 
 	connectionInfo := exec.Connection
@@ -86,10 +122,35 @@ func formatCopyArgToScpArgs(ctx context.Context, argExecID string, exec *types.E
 		return "", fmt.Errorf("Could not get connection from session")
 	}
 
-	parts := strings.SplitN(arg, "/", 2)
-	dir := filepath.Clean(parts[1])
+	return fmt.Sprintf("%s@%s:%s", connectionInfo.User, connectionInfo.Host, splitSessFromDirpath(arg)), nil
+}
 
-	return fmt.Sprintf("%s@%s:/%s", connectionInfo.User, connectionInfo.Host, dir), nil
+func formatPaths(exec *types.Exec, args []string) ([]string, error) {
+	formattedArgs := make([]string, len(args))
+
+	for i, arg := range args {
+		if strings.HasPrefix(arg, "sess:") {
+			formattedArg, err := formatRemotePath(exec, arg)
+			if err != nil {
+				return nil, err
+			}
+			formattedArgs[i] = formattedArg
+		} else {
+			formattedArg, err := formatLocalPath(arg)
+			if err != nil {
+				return nil, err
+			}
+			formattedArgs[i] = formattedArg
+		}
+	}
+
+	return formattedArgs, nil
+}
+
+// splitSessFromDirpath takes a qualified cp argument e.g. sess:<execId>/path and returns /path
+func splitSessFromDirpath(arg string) string {
+	parts := strings.SplitN(arg, "/", 2)
+	return fmt.Sprintf("/%s", filepath.Clean(parts[1]))
 }
 
 func getExecIDFromCopyArgs(input string) (string, error) {
